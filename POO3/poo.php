80711368f9191d6f38d1f6f4a381bf000be0f1d0
user <sami.elnakadi@gmail.com>
2019-04-26 15:22:54 +0200

    POO3

--- POO3/poo.php
+++ POO3/poo.php
@@ -5,16 +5,16 @@ class Validator{
         echo filter_var ( $text, FILTER_SANITIZE_STRING);
     }
 
-    public function sanitizeEntier($text){
-        echo filter_var ( $text, FILTER_SANITIZE_NUMBER_INT);
+    public function sanitizeEntier($number){
+        echo filter_var ( $number, FILTER_SANITIZE_NUMBER_INT);
     }
 
-    public function sanitizeDecimal($text){
-        echo filter_var ( $text, FILTER_SANITIZE_FLOAT);
+    public function sanitizeDecimal($deci){
+        echo filter_var ( $tdeci, FILTER_SANITIZE_FLOAT);
     }
 
-    public function sanitizeMail($text){
-        echo filter_var ( $text, FILTER_SANITIZE_EMAIL);
+    public function sanitizeMail($mail){
+        echo filter_var ( $mail, FILTER_SANITIZE_EMAIL);
     }
 }
 ?>
